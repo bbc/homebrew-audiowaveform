c8849d1e3533b6f37ddb7444e7c8f3e5a0eb173d
Chris Needham <chris.needham@bbc.co.uk>
2016-02-25 10:40:49 +0000

    Replaced tabs with spaces

--- audiowaveform.rb
+++ audiowaveform.rb
@@ -1,37 +1,37 @@
 require 'formula'
 
 class Audiowaveform < Formula
-	desc "C++ program to generate waveform data and render waveform images from audio files"
-	homepage "https://github.com/bbcrd/audiowaveform"
-	url "https://github.com/bbcrd/audiowaveform/archive/1.0.10.tar.gz"
-	sha256 "7d76a65962ec147befaad807aeae893a0d6f7f8443f9a5ecfe4776cbfc8cff37"
+  desc "C++ program to generate waveform data and render waveform images from audio files"
+  homepage "https://github.com/bbcrd/audiowaveform"
+  url "https://github.com/bbcrd/audiowaveform/archive/1.0.10.tar.gz"
+  sha256 "7d76a65962ec147befaad807aeae893a0d6f7f8443f9a5ecfe4776cbfc8cff37"
 
-	depends_on "cmake" 
-	depends_on "libmad"
-	depends_on "libsndfile"
-	depends_on "gd"
-	if MacOS.version < :mavericks
-		depends_on "boost" => "c++11"
-	else
-		depends_on "boost"
-	end
-	# depends_on "gmock"
+  depends_on "cmake"
+  depends_on "libmad"
+  depends_on "libsndfile"
+  depends_on "gd"
 
-	def install
-		cmake_args = std_cmake_parameters.split
-		cmake_args << "-DENABLE_TESTS=0"
-		cmake_args << "-DCMAKE_C_COMPILER=/usr/bin/clang"
-		cmake_args << "-DCMAKE_CXX_COMPILER=/usr/bin/clang++"
-		cmake_args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+  if MacOS.version < :mavericks
+    depends_on "boost" => "c++11"
+  else
+    depends_on "boost"
+  end
+  # depends_on "gmock"
 
-		mkdir "build" do
-			system "cmake", "..", *cmake_args
-			system "make", "install"
-		end
-	end
+  def install
+    cmake_args = std_cmake_parameters.split
+    cmake_args << "-DENABLE_TESTS=0"
+    cmake_args << "-DCMAKE_C_COMPILER=/usr/bin/clang"
+    cmake_args << "-DCMAKE_CXX_COMPILER=/usr/bin/clang++"
+    cmake_args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
 
-	test do
-		# system "make", "test"
-	end
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    # system "make", "test"
+  end
 end
-
