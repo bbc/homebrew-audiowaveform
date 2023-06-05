5a9089d1b2e522407fc9a88b3d9734119953dde2
Chris Needham <chris@chrisneedham.com>
2023-06-05 20:39:50 +0100

    Simplified update script

--- update.sh
+++ update.sh
@@ -11,8 +11,8 @@ then
     exit 1
 fi
 
-FILENAME="${VERSION}.tar.gz"
-URL="https://github.com/bbc/audiowaveform/archive/$FILENAME"
+FILENAME=$VERSION.tar.gz
+URL=https://github.com/bbc/audiowaveform/archive/$FILENAME
 
 if [ -e $FILENAME ]
 then
@@ -29,6 +29,6 @@ echo "sha256: $SHASUM"
 FORMULA_FILENAME=audiowaveform.rb
 
 sed -i "s|url \".*\"|url \"$URL\"|" $FORMULA_FILENAME
-sed -i "s|sha256 \".*\"|sha256 \"${SHASUM}\"|" $FORMULA_FILENAME
+sed -i "s|sha256 \".*\"|sha256 \"$SHASUM\"|" $FORMULA_FILENAME
 
 echo "Updated $FORMULA_FILENAME"
